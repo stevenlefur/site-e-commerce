6e1bf95258d94c25aff534be733125d6172cadf4
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-15 12:06:41 +0200

    Fonction remove item sur button. Icon non OK.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,13 @@
 //     $("#cartModal").modal("show");
 //   });
   
+let removeCartItemButtons = document.getElementsByClassName('btn-danger');
+console.log(removeCartItemButtons);
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i];
+    button.addEventListener('click', function(event){
+        let buttonClicked = event.target;
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}
+/* La fonction remove ne fonctionne pas sur l'icône*/
