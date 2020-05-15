571cc9dd741f7980e6de6d439f62eff3ffdf0b75
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-15 12:51:36 +0200

    Fonction updateTotal nOK.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 // $(document).ready(function () {
 //     $("#cartModal").modal("show");
 //   });
-  
+
 let removeCartItemButtons = document.getElementsByClassName('btn-danger');
 console.log(removeCartItemButtons);
 for (let i = 0; i < removeCartItemButtons.length; i++) {
     let button = removeCartItemButtons[i];
-    button.addEventListener('click', function(event){
+    button.addEventListener('click', function (event) {
         let buttonClicked = event.target;
         buttonClicked.parentElement.parentElement.remove()
+        updateCardTotal();
     })
 }
 /* La fonction remove ne fonctionne pas sur l'icône*/
+
+function updateCardTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    let cartItems = document.getElementsByClassName('cart-item');
+    let total = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+        let cartItem = cartItems[0];
+        let priceElement = cartItem.getElementsByClassName('cart-price')[0];
+        let quantityElement = cartItem.getElementsByClassName('form-control')[0];
+        let price = parseFloat(priceElement.innerText.replace('€', ''));
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('price')[0].innerText = total;
+
+}
