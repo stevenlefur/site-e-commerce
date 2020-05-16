8ac78bc0f35e8eaf1a5e3d4e81a92c03da4a6cee
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-16 22:00:37 +0200

    Icones sur boutton panier fonctionnels.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@
 
 let removeCartItemButtons = document.getElementsByClassName('btn-danger');
 console.log(removeCartItemButtons);
+
 for (let i = 0; i < removeCartItemButtons.length; i++) {
     let button = removeCartItemButtons[i];
-    button.addEventListener('click', function (event) {
-        let buttonClicked = event.target;
-        buttonClicked.parentElement.parentElement.remove()
+    button.addEventListener('click', function () {
+        this.closest("tr").remove();
         updateCardTotal();
     })
 }
