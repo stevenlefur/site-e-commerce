0b5d9a05a0757b3efeed52a48d865853ceb29547
HeleneGuibaud <62548550+HeleneGuibaud@users.noreply.github.com>
2020-05-18 12:31:04 +0200

    Mise à jour du nombre dans le panier et création du sous-total.

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,20 +61,20 @@ function addToCartClicked() {
     let imgSrc = shopItem.getElementsByClassName('picture')[0].src;
     let product = shopItem.getElementsByClassName('name')[0].innerText;
     let price = shopItem.getElementsByClassName('price')[0].innerText;
-    addItemToCart(imgSrc, product, price);
+    addItemToCart(imgSrc, product, price, price);
     updateCardTotal();
 }
 
-function addItemToCart(imgSrc, product, price) {
+function addItemToCart(imgSrc, product, subTotal, price) {
     let cartItem = document.createElement('tr');
     cartItem.classList.add('cart-item');
     let cartItems = document.getElementsByClassName('cart-items')[0];
     let cartItemsNames = cartItems.getElementsByClassName('cart-product');
     for (let i = 0; i < cartItemsNames.length; i++) {
-        if (cartItemsNames[i].innerText== product) {
+        if (cartItemsNames[i].innerText == product) {
             alert('Le produit selectionné a déjà été ajouté au panier.');
             return
-        }    
+        }
     }
     let cartItemContents = `
         <td class="w-25">
@@ -85,13 +85,13 @@ function addItemToCart(imgSrc, product, price) {
         <td class="cart-price">${price}</td>
         <td class="qty"><input type="text" class="form-control" id="input1" value="1">
         </td>
-        <td>1</td>
+        <td class="sub-total">${subTotal}</td>
         <td>
             <a href="#" class="btn btn-danger btn-sm">
                 <i class="fa fa-times"></i>
             </a>
         </td>`
-        cartItem.innerHTML = cartItemContents;
+    cartItem.innerHTML = cartItemContents;
     cartItems.append(cartItem);
     cartItem.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
     cartItem.getElementsByClassName('qty')[0].addEventListener('change', quantityChanged);
@@ -101,14 +101,27 @@ function updateCardTotal() {
     let cartItemContainer = document.getElementsByClassName('cart-items')[0];
     let cartItems = cartItemContainer.getElementsByClassName('cart-item');
     let total = 0;
+    let qteTotal = 0;
     for (let i = 0; i < cartItems.length; i++) {
+        // Création de variables.
         let cartItem = cartItems[i];
         let priceElement = cartItem.getElementsByClassName('cart-price')[0];
         let quantityElement = cartItem.getElementsByClassName('form-control')[0];
         let price = parseFloat(priceElement.innerText.replace('€', ''));
         let quantity = quantityElement.value;
-        total = total + (price * quantity);
+
+        // Calcul sous total de la carte.
+        qteTotal = qteTotal + parseInt(quantity);
+        let subTotal = price * quantity;
+        subTotal = Math.round(subTotal * 100) / 100;
+        cartItem.getElementsByClassName('sub-total')[0].innerText = subTotal + '€';
+
+        // Calcul du total.
+        total = total + subTotal;
     }
     total = Math.round(total * 100) / 100;
     document.getElementsByClassName('price')[0].innerText = total + '€';
+
+    // Mise à jour du badge.s
+    document.getElementsByClassName('badge')[0].innerText = qteTotal;
 }
